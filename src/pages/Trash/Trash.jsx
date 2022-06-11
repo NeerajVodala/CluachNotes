3f4322dfc18e88054ba14eb62f780e822ac6e317
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-06-11 15:16:26 +0530

    style: default trash page

--- src/pages/Trash/Trash.jsx
+++ src/pages/Trash/Trash.jsx
@@ -1,5 +1,10 @@
 import "./Trash.css";
 
 export const Trash = () => {
-  return <>This is Trash</>;
+  return (
+    <div className="trash flex-col justify-center align-center gp-2xl text-center">
+      <i className="fas fa-trash fa-6x text-span-2"></i>
+      <p className="text-xl text-span-1">No notes in trash</p>
+    </div>
+  );
 };
