d0eeb868dc2f828675651c77c622f2db18d537be
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-06-12 22:53:22 +0530

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
 import "./Auth.css";
 
 export const Login = () => {
-  return <>This is Login</>;
+  return (
+    <main className="auth-page flex-row align-center justify-center">
+      <div className="auth-container flex-row align-center justify-center shadow-1 br-s">
+        <div className="input-section">
+          <h3>Login</h3>
+          <div className="input-container">
+            <label htmlFor="email" className="label">
+              Email address
+            </label>
+            <input
+              type="email"
+              placeholder="email@example.com"
+              className="input"
+              id="email"
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              className="input"
+              id="password"
+            />
+          </div>
+          <div className="flex-row align-center justify-center gp-2xl">
+            <div className="flex-row align-center gp-s">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me" className="label text-s">
+                {" "}
+                Remember me
+              </label>
+            </div>
+            <div className="text-s">Forgot your Password?</div>
+          </div>
+          <button className="btn btn-m solid br-s">Login</button>
+          <div className="text-center text-m">
+            <Link to="/signup">Create New Account</Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
