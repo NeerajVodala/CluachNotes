b4f1169067ec2baf926c486381e544a13fdd4f28
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-06-24 12:11:04 +0530

    Update Trash.jsx

--- src/pages/Trash/Trash.jsx
+++ src/pages/Trash/Trash.jsx
@@ -1,10 +1,15 @@
+import { Sidebar } from "../../components";
 import "./Trash.css";
+import "../../styles/App.css";
 
 export const Trash = () => {
   return (
-    <div className="trash flex-col justify-center align-center gp-2xl text-center">
-      <i className="fas fa-trash fa-6x text-span-2"></i>
-      <p className="text-xl text-span-1">No notes in trash</p>
+    <div className="main">
+      <Sidebar />
+      <div className="trash flex-col justify-center align-center gp-2xl text-center">
+        <i className="fas fa-trash fa-6x text-span-2"></i>
+        <p className="text-xl text-span-1">No notes in trash</p>
+      </div>
     </div>
   );
 };
