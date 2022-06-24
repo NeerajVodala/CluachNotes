c0edf77ddd525cf7657988cb57a91504286369ae
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-06-24 12:10:44 +0530

    Update Archive.jsx

--- src/pages/Archive/Archive.jsx
+++ src/pages/Archive/Archive.jsx
@@ -1,10 +1,15 @@
 import "./Archive.css";
+import "../../styles/App.css";
+import { Sidebar } from "../../components";
 
 export const Archive = () => {
   return (
-    <div className="archive flex-col justify-center align-center gp-2xl text-center">
-      <i className="fas fa-file-archive fa-6x text-span-2"></i>
-      <p className="text-xl text-span-1">Your archived notes appear here</p>
+    <div className="main">
+      <Sidebar />
+      <div className="archive flex-col justify-center align-center gp-2xl text-center">
+        <i className="fas fa-file-archive fa-6x text-span-2"></i>
+        <p className="text-xl text-span-1">Your archived notes appear here</p>
+      </div>
     </div>
   );
 };
