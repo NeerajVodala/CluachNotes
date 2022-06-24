2b9c959846c10a6a2806584d85d5332207147ddd
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-06-24 12:10:57 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,28 @@
-import { NotesCard, TextEditor } from "../../components";
+import { NotesCard, TextEditor, Sidebar } from "../../components";
 import "./Home.css";
+import "../../styles/App.css";
 
 export const Home = () => {
   return (
-    <div className="">
-      <TextEditor />
-      <div>
-        <p className="text-span-1 text-s text-semibold">PINNED</p>
-        <div className="flex-row flex-wrap">
-          <NotesCard />
-          <NotesCard />
+    <div className="main">
+      <Sidebar />
+      <div className="home">
+        <TextEditor />
+        <div>
+          <p className="text-span-1 text-s text-semibold">PINNED</p>
+          <div className="flex-row flex-wrap">
+            <NotesCard />
+            <NotesCard />
+          </div>
         </div>
-      </div>
-      <div>
-        <p className="text-span-1 text-s text-semibold">OTHERS</p>
-        <div className="flex-row flex-wrap">
-          <NotesCard />
-          <NotesCard />
-          <NotesCard />
-          <NotesCard />
+        <div>
+          <p className="text-span-1 text-s text-semibold">OTHERS</p>
+          <div className="flex-row flex-wrap">
+            <NotesCard />
+            <NotesCard />
+            <NotesCard />
+            <NotesCard />
+          </div>
         </div>
       </div>
     </div>
