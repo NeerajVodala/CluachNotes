edc22bea0128a37ca0fb0b040b2216f6336144df
Neeraj Vodala <vodalaneeraj@gmail.com>
2022-07-30 00:54:22 +0530

    fix: filter notes before render

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,35 +8,34 @@ export const Home = () => {
   const { notesState } = useNote();
   const { pathname } = useLocation();
 
+  const pinnedNotes = notesState.notesList?.filter((note) => note.isPinned);
+  const otherNotes = notesState.notesList?.filter((note) => !note.isPinned);
+
   return (
     <div className="main">
       <Sidebar />
       <div className="home">
         <TextEditor />
-        <div>
-          <p className="text-span-1 text-s text-semibold">PINNED</p>
-          <div className="flex-row flex-wrap">
-            {notesState.noteList?.map((n) =>
-              n.isPinned ? (
+        {pinnedNotes.length > 0 && (
+          <div>
+            <p className="text-span-1 text-s text-semibold">PINNED</p>
+            <div className="flex-row flex-wrap">
+              {pinnedNotes.map((n) => (
                 <NotesCard Note={n} pathname={pathname} key={n._id} />
-              ) : (
-                <></>
-              )
-            )}
+              ))}
+            </div>
           </div>
-        </div>
-        <div>
-          <p className="text-span-1 text-s text-semibold">OTHERS</p>
-          <div className="flex-row flex-wrap">
-            {notesState.noteList?.map((n) =>
-              !n.isPinned ? (
+        )}
+        {otherNotes.length > 0 && (
+          <div>
+            <p className="text-span-1 text-s text-semibold">ALL NOTES</p>
+            <div className="flex-row flex-wrap">
+              {otherNotes.map((n) => (
                 <NotesCard Note={n} pathname={pathname} key={n._id} />
-              ) : (
-                <></>
-              )
-            )}
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
